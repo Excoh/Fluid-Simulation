04f0471a7e6c82c6923c2b088e0c4e82e54c59a1
Excoh <excel.ortega@gmail.com>
2018-02-25 05:02:11 -0700

    Started Project

--- Fluid Simulation/Program.cs
+++ Fluid Simulation/Program.cs
@@ -14,7 +14,7 @@ namespace Fluid_Simulation
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Main())
                 game.Run();
         }
     }
